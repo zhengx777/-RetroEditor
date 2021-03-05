759773b4e8a19a0e9cbf046d5ac1c2b5db6f1030
zhengxin213221 <zhengxin213221@sogou-inc.com>
2021-03-05 19:38:27 +0800

    init

---
+++ src/App.svelte
@@ -0,0 +1,53 @@
+<script>
+    import Editor from "./editor/main.js";
+    import { onMount } from "svelte";
+    onMount(() => {
+        const editorContainer = document.getElementById("editorContainer");
+        window.editor = new Editor(editorContainer);
+    });
+    function execBold() {
+        window.editor.execBold();
+    }
+</script>
+
+<main>
+    <div id="toolBar">
+        <span class="toolButton" on:click={execBold}>
+            <div class="menu-item">加粗</div>
+        </span>
+        <span class="toolButton">
+            <div class="menu-item">斜体</div>
+        </span>
+        <span class="toolButton">
+            <div class="menu-item">一级标题</div>
+        </span>
+        <span class="toolButton">
+            <div class="menu-item">二级标题</div>
+        </span>
+        <span class="toolButton">
+            <div class="menu-item">超链接</div>
+        </span>
+        <span class="toolButton">
+            <div class="menu-item">图片</div>
+        </span>
+        <span class="toolButton">
+            <div class="menu-item">撤销</div>
+        </span>
+        <span class="toolButton">
+            <div class="menu-item">重做</div>
+        </span>
+    </div>
+    <div id="editorContainer" />
+</main>
+
+<style>
+    main {
+        width: 800px;
+        margin: auto;
+    }
+    .toolButton {
+        display: inline-block;
+        padding: 2px 5px;
+        cursor: pointer;
+    }
+</style>
