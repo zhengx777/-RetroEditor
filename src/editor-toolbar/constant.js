7fdce6fb1205075a695b1dced26ef5ff27bfcf36
zhengx777 <zhengx666@gmail.com>
2021-06-28 19:51:59 +0800

    update

--- src/editor-toolbar/constant.js
+++ src/editor-toolbar/constant.js
@@ -1,54 +1,147 @@
 const ButtonList = [
     {
-        title: "clean",
-        exec: "",
-        des: ""
-    },
-    {
-        title: "print",
-        exec: "",
-        des: ""
-    },
-    {
+        id: 0,
         title: "undo",
-        exec: "",
-        des: ""
+        exec: "undo",
+        des: "Undo"
     },
     {
+        id: 1,
         title: "redo",
-        exec: "",
-        des: ""
+        exec: "redo",
+        des: "Redo"
     },
     {
-        title: "removeFormatting",
-        exec: "",
-        des: ""
+        id: 2,
+        title: "removeFormat",
+        exec: "removeFormat",
+        des: "Remove formatting"
     },
     {
+        id: 3,
         title: "bold",
-        exec: "",
-        des: ""
+        exec: "bold",
+        des: "Bold"
     },
     {
+        id: 4,
         title: "italic",
-        exec: "",
-        des: ""
+        exec: "italic",
+        des: "Italic"
     },
     {
+        id: 5,
         title: "underline",
-        exec: "",
-        des: ""
+        exec: "underline",
+        des: "Underline"
     },
     {
-        title: "clean",
-        exec: "",
-        des: ""
+        id: 6,
+        title: "justifyleft",
+        exec: "justifyleft",
+        des: "Left align"
     },
     {
-        title: "clean",
-        exec: "",
-        des: ""
+        id: 7,
+        title: "justifycenter",
+        exec: "justifycenter",
+        des: "Center align"
     },
+    {
+        id: 8,
+        title: "justifyright",
+        exec: "justifyright",
+        des: "Right align"
+    },
+    {
+        id: 9,
+        title: "insertorderedlist",
+        exec: "insertorderedlist",
+        des: "Numbered list"
+    },
+    {
+        id: 10,
+        title: "insertunorderedlist",
+        exec: "insertunorderedlist",
+        des: "Dotted list"
+    },
+    {
+        id: 11,
+        title: "blockquote",
+        exec: "blockquote",
+        des: "Quote"
+    },
+    {
+        id: 12,
+        title: "outdent",
+        exec: "outdent",
+        des: "Add indentation"
+    },
+    {
+        id: 13,
+        title: "indent",
+        exec: "indent",
+        des: "Delete indentation"
+    },
+    {
+        id: 14,
+        title: "createlink",
+        exec: "createlink",
+        des: "Hyperlink"
+    },
+    {
+        id: 15,
+        title: "copy",
+        exec: "copy",
+        des: "Copy"
+    },
+    {
+        id: 16,
+        title: "cut",
+        exec: "cut",
+        des: "Cut"
+    },
+    {
+        id: 17,
+        title: "paste",
+        exec: "paste",
+        des: "Paste"
+    },
+    {
+        id: 18,
+        title: "formatblock",
+        exec: "formatblock",
+        des: "Formatblock",
+        secondArgument: true
+    },
+    {
+        id: 19,
+        title: "fontname",
+        exec: "fontname",
+        des: "Fontname",
+        secondArgument: true
+    },
+    {
+        id: 20,
+        title: "fontsize",
+        exec: "fontsize",
+        des: "Fontsize",
+        secondArgument: true
+    },
+    {
+        id: 21,
+        title: "forecolor",
+        exec: "forecolor",
+        des: "Forecolor",
+        secondArgument: true
+    },
+    {
+        id: 22,
+        title: "backcolor",
+        exec: "backcolor",
+        des: "Backcolor",
+        secondArgument: true
+    }
 ];
 
 export {
--- src/editor-toolbar/index.js
+++ src/editor-toolbar/index.js
@@ -1,8 +1,37 @@
+import { ButtonList } from './constant';
+import ToolBarStyle from './toolBar.css';
 class ToolBar {
-    constructor() {
+    constructor(selectors, options) {
+        if (!selectors) {
+            throw new Error(`新建工具栏实例必须提供节点选择器!`)
+        }
 
+        if (!window.retroEditor) {
+            throw new Error(`新建工具栏需要先实例化Retro Editor!`)
+        }
+
+        const toolBarDom = document.querySelector(selectors);
+
+        if (!toolBarDom) {
+            throw new Error(`无效的节点选择器'${selectors}'!`);
+        }
+
+        this.root = toolBarDom;
+
+        this.options = options ? options : {};
+
+        this.initToolBar();
     }
-    init() {
-        const toolBar
+
+    initToolBar() {
+        const toolBarContainer = document.createElement("div");
+        toolBarContainer.setAttribute("class", "retro-editor-toolbar");
+
+        ButtonList.forEach(item => {
+            const span = document.createElement("span");
+            span.innerText = item.des;
+            span.setAttribute("data-exec", item.exec);
+            span.setAttribute("class", "toolbar-item");
+        })
     }
 }
